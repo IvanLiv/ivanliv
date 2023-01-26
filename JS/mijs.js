ede4bbff803300ecf4ffd02d5efe15457576e667
IvanLiv <elunico_mrl@hotmail.com>
2023-01-26 20:42:16 -0300

    2023_01_26

---
+++ JS/mijs.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("email");
+    document.getElementById("demo").innerHTML ="blabla@bla.com";
+}
+  
+document.getElementById("boton1").onclick = function () {
+    console.log("Celular");
+    document.getElementById("demo1").innerHTML ="555-5555-5555";
+}
+  
+document.getElementById("boton2").onclick = function () {
+    console.log("Ubicación");
+    document.getElementById("demo2").innerHTML ="Buenos Aires, Argentina";
+}
